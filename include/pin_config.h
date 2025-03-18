8e8514237c420288f20034200cb7f224cd6924cb
Squynijos <81109473+Squynijos@users.noreply.github.com>
2025-03-18 14:11:26 -0400

    BME interne

--- include/pin_config.h
+++ include/pin_config.h
@@ -1,18 +1,32 @@
 #pragma once
 #include "pins_arduino.h"
+#include "driver/uart.h"
 
+//--------- PINS DEF ---------
+#define UART_DBG UART_NUM_0
+#define UART_RS485 UART_NUM_2
+#define UART_SW UART_NUM_2
+
+const int TX_DEBUG = 1;
+const int RX_DEBUG = 3;
+const int TX_SWITCH = 17;
+const int RX_SWITCH = 16;
+const int TX_RS485 = 4;
+const int RX_RS485 = 12;
+
+/**
+ * Classe contenant les fonctions de configuration des Pins
+ */
 class Pin
 {
 public:
-#define TX_DEBUG 1
-#define RX_DEBUG 3
-#define TX_SWITCH 17
-#define RX_SWITCH 16
-#define TX_RS485 4
-#define RX_RS485 12
+  /**
+   * Constucteur de logique des Pins
+   */
+  Pin();
 
   /**
    * Exécute la configuration hardware des pins du Firebeetle
    */
-  static void configurePins();
+  void configurePins();
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,42 @@
+//---------- INCLUDES ----------
+
 #include <Arduino.h>
 #include "pin_config.h"
+#include "Adafruit_BME280.h"
+#include "Sodaq_LSM303AGR.h"
 
+//---------- ADRESSES ---------
+
+#define ADDR_BME 0x76
+#define ADDR_LSM 0x1E
+
+//---------- OBJETS ---------
+
+Adafruit_BME280 bme280;
+Sodaq_LSM303AGR lsm303;
+
+//---------- MAIN PROGRAM ---------
 void setup()
 {
-  Pin::configurePins();
+  // configurePins();
+
+  // Initialisation
+  Serial.begin(115200);
+  delay(500);
+
+  bme280.begin(ADDR_BME);
+  lsm303.disableMagnetometer();
+
+  // Logique
+  // Serial.println(bme280.readTemperature());
 }
 
 void loop()
 {
+  Serial.println(bme280.readTemperature());
+  Serial.println(bme280.readPressure());
+  Serial.println(bme280.readHumidity());
+  Serial.println();
+  delay(500);
+  // No-Op
 }

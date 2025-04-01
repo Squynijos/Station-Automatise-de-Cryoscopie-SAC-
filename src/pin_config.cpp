1eb4572facdb75a5a41aac266c0bdc07e3829774
Squynijos <81109473+Squynijos@users.noreply.github.com>
2025-04-01 14:55:43 -0400

    FACTORY RESET

--- src/pin_config.cpp
+++
@@ -1,9 +0,0 @@
-#include "pin_config.h"
-#include "driver/uart.h"
-
-
-void Pin::configurePins()
-{
-  // uart_set_pin(UART_RS485, TX_RS485, RX_RS485, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
-  // uart_set_pin(UART_SW, TX_SWITCH, RX_SWITCH, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
-}
